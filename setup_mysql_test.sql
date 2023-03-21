44fa3fc928d670050d9a101a19c19cdea9c7546e
surafel27 <surafelfekadu399@gmail.com>
2023-03-21 07:42:44 +0000

    creating DB, USER and Granting privilage

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,10 +3,10 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- selecting the database
 USE hbnb_test_db;
--- creating a user 'hbnb_dev'
+-- creating a user 'hbnb_test'
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- granting privilages
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 -- reloading the grant table
 FLUSH PRIVILEGES;
