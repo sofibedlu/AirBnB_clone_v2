ef21d29edafd7b95744c7b73d672fde2aae78841
surafel27 <surafefekadu399@gmail.com.com>
2023-03-22 05:08:57 -0700

    creating user

--- models/user.py
+++ models/user.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
-
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    if models.db_store == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    def __init__(self, *args, **kwargs):
+        """initalize user class"""
+        super().__init__(*args, **kwargs)
