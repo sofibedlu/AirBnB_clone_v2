4981389b0dbd6fc6330f18e4b635fe617f7f0f91
sofi <bedlusofi@gmail.com>
2023-04-07 18:15:12 +0000

    update-02

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,29 +8,29 @@ then
 fi
 if [ ! -d "/data/" ]
 then
-        sudo mkdir -p /data/
+        sudo mkdir /data/
 fi
 if [ ! -d "/data/web_static/" ]
 then
-        sudo mkdir -p /data/web_static/
+        sudo mkdir /data/web_static/
 fi
 if [ ! -d "/data/web_static/releases/" ]
 then
-        sudo mkdir -p /data/web_static/releases/
+        sudo mkdir /data/web_static/releases/
 fi
 if [ ! -d "/data/web_static/shared/" ]
 then
-        sudo mkdir -p /data/web_static/shared/
+        sudo mkdir /data/web_static/shared/
 fi
 if [ ! -d "/data/web_static/releases/test/" ]
 then
-        sudo mkdir -p /data/web_static/releases/test/
+        sudo mkdir /data/web_static/releases/test/
 fi
 
 sudo echo "test nginx config/hello_world" | sudo tee /data/web_static/releases/test/index.html
 
 link="/data/web_static/current"
-target="/data/web_static/releases/test/"
+target="/data/web_static/releases/test"
 if [ -e "$link" ]
 then
         sudo rm /data/web_static/current
