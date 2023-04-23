80cdc8b37d80f185e4fb54db1887bb71788c865c
sofi <bedlusofi@gmail.com>
2023-04-23 14:44:07 +0000

    sort

---
+++ web_flask/9-states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""starts flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+states = storage.all(State)
+cities = storage.all(City)
+
+
+@app.teardown_appcontext
+def cleanup(exc):
+    """remove current sqlalchemy session"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def city_state():
+    """display html page"""
+    return render_template('8-cities_by_states.html', states=states, cities=cities)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
