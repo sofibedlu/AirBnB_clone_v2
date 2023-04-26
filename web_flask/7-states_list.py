8e32644d42a64c16601a4a31a2ef1ac1e9cb77b4
sofi <bedlusofi@gmail.com>
2023-04-26 17:47:06 +0000

    chek-1-doc

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,8 +2,6 @@
 """starts flask web application
     -use storage to fetch data from the storage engine
     -after each request it removes th current sqlalchemy session
-
-
 """
 from flask import Flask, render_template
 from models import storage
@@ -11,7 +9,6 @@ from models.state import State
 
 
 app = Flask(__name__)
-states = storage.all(State)
 
 
 @app.teardown_appcontext
@@ -23,6 +20,7 @@ def cleanup(exc):
 @app.route('/states_list', strict_slashes=False)
 def state():
     """display html page"""
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
