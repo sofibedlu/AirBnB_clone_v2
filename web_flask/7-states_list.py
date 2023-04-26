9f92b29d36523428ce0d59b6c310138467e9d83e
sofi <bedlusofi@gmail.com>
2023-04-26 17:41:31 +0000

    document updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""starts flask web application"""
+"""starts flask web application
+    -use storage to fetch data from the storage engine
+    -after each request it removes th current sqlalchemy session
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
